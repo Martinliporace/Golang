87703aaa485e246174b3bf7155742bf6f6192886
Martinliporace <98000916+Martinliporace@users.noreply.github.com>
2022-04-19 20:04:30 -0300

    Add files via upload

---
+++ 0x01/3-print_alphabets.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+    for i := 'a'; i <= 'z'; i++{
+            fmt.Print(string(i))
+	}
+
+	for j := 'A'; j <= 'Z'; j++{
+            fmt.Print(string(j))
+	}
+		    
+}
